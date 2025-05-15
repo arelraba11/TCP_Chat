b1883ac6be5d5a0e07675e30ad18fabdac43c19f
Arel Raba <arelraba11@gmail.com>
2025-05-15 10:28:07 +0000

    Refactor server code: improve structure, cleanup includes, add nickname lookup utility

--- client.c
+++ client.c
@@ -1,4 +1,3 @@
-#include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,10 +7,12 @@
 #include <pthread.h>
 
 #define PORT 1337
+#define NICKNAME_LEN 50
+#define BUFFER_SIZE 1024
 
 void *receive_handler(void *arg) {
 	int sock = *(int *) arg;
-	char buffer[1024];
+	char buffer[BUFFER_SIZE];
 
 	while(1) {
 		ssize_t n = read(sock, buffer, sizeof(buffer));
@@ -20,7 +21,7 @@ void *receive_handler(void *arg) {
 			close(sock);
 			exit(0);
 		}
-		printf("[CHAT] %s", buffer);
+		printf("[CHAT] %s\n", buffer);
 	}
 	return NULL;
 }
@@ -28,44 +29,51 @@ void *receive_handler(void *arg) {
 int main(void) {
 	int sock;
 	struct sockaddr_in server_name;
-	size_t len;
-	char buffer[1024];
-	char nickname[50];
+	char buffer[BUFFER_SIZE];
+	char nickname[NICKNAME_LEN];
 
 	sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock < 0) {
-		perror("Error opening channel");
+		perror("[CLIENT] Error opening channel");
 		exit(1);
 	}
 
-	bzero(&server_name, sizeof(server_name));
+	memset(&server_name, 0, sizeof(server_name));
 	server_name.sin_family = AF_INET;
 	server_name.sin_addr.s_addr = htonl(INADDR_LOOPBACK); // 127.0.0.1
 	server_name.sin_port = htons(PORT);
 
 	printf("Please enter your nickname: ");
-	fgets(nickname, sizeof(nickname), stdin);
-
-	if (strlen(nickname) == 0) {
-		printf("Invaild nickname. Exiting..\n");
+	if(fgets(nickname, sizeof(nickname), stdin) == NULL) {
+		printf("[CLIENT] Failed to read nickname. Exiting...\n");
 		exit(1);
 	}
-
 	nickname[strcspn(nickname, "\n")] = 0;
 
-	if(connect(sock, (struct sockaddr *)&server_name, sizeof(server_name)) < 0) {
-		perror("Error establishing communications");
+	if (strlen(nickname) == 0) {
+		printf("[CLIENT] Invaild nickname. Exiting..\n");
 		exit(1);
 	}
 
-	write(sock, nickname, strlen(nickname) + 1);
+	if(connect(sock, (struct sockaddr *)&server_name, sizeof(server_name)) < 0) {
+		perror("[CLIENT] Error establishing connection to server");
+		exit(1);
+	}
+
+	if (write(sock,nickname, strlen(nickname) + 1) <= 0) {
+		perror("[CLIENT] Failed to send nickname to server");
+		close(sock);
+		exit(1);
+	}
 
 	pthread_t tid;
 	pthread_create(&tid, NULL, receive_handler, &sock);
 
-	while (1) {
-		if(fgets(buffer, sizeof(buffer), stdin) == NULL) break;
-		if(write(sock, buffer, strlen(buffer) + 1) <= 0) break;
+	while (fgets(buffer, sizeof(buffer), stdin) != NULL) {
+		if(write(sock, buffer, strlen(buffer) + 1) <= 0) {
+			printf("[CLIENT] Failed to send message. Exiting...\n");
+			break;
+		}
 	}
 
 	close(sock);
--- server.c
+++ server.c
@@ -1,32 +1,45 @@
-#include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <sys/socket.h>
-#include <sys/un.h>
 #include <netinet/in.h>
 #include <pthread.h>
 
 #define PORT 1337
 #define MAX_CLIENTS 100
-int sock;
+#define NICKNAME_LEN 50
+#define BUFFER_SIZE 1024
+
 typedef struct {
 	int socket;
 	char nickname[50];
 } Client;
+
 Client clients[MAX_CLIENTS];
 int client_count = 0;
+int server_socket;
 pthread_mutex_t client_lock = PTHREAD_MUTEX_INITIALIZER;
+
+const char* get_nickname(int socket) {
+	for(int i = 0; i < client_count; i++) {
+		if(clients[i].socket == socket) {
+			return clients[i].nickname;
+		}
+	}
+	return "Unknown";
+}
 
 void add_client(int socket, char *nickname) {
 	pthread_mutex_lock(&client_lock);
-	if(client_count < MAX_CLIENTS){
-		clients[client_count].socket = socket;
-		strncpy(clients[client_count].nickname, nickname, sizeof(clients[client_count].nickname));
-		client_count++;
+	if(client_count >= MAX_CLIENTS){
+		pthread_mutex_unlock(&client_lock);
+		return;
 	}
+	clients[client_count].socket = socket;
+	strncpy(clients[client_count].nickname, nickname, NICKNAME_LEN);
+	client_count++;
 	pthread_mutex_unlock(&client_lock);
 }
 
@@ -43,17 +56,15 @@ void remove_client(int socket) {
 
 void broadcast_message(char *msg, int sender_socket) {
 	pthread_mutex_lock(&client_lock);
-	char final_msg[1084];
 
-	const char *sender_nick = "Unknown";
-	for(int i = 0; i < client_count; i++) {
-		if (clients[i].socket == sender_socket)	sender_nick = clients[i].nickname;
-	}
-
+	char final_msg[BUFFER_SIZE + NICKNAME_LEN + 10];
+	const char *sender_nick = get_nickname(sender_socket);
 	snprintf(final_msg, sizeof(final_msg), "%s: %s", sender_nick, msg);
 
 	for(int i = 0; i < client_count; i++) {
-		if(clients[i].socket != sender_socket) write(clients[i].socket, final_msg, strlen(final_msg) +1);
+		if (clients[i].socket != sender_socket) {
+			write(clients[i].socket, final_msg, strlen(final_msg) + 1);
+		}
 	}
 
 	pthread_mutex_unlock(&client_lock);
@@ -62,26 +73,25 @@ void broadcast_message(char *msg, int sender_socket) {
 void *handle_client(void *arg) {
 	int client_socket =  *((int *) arg);
 	free(arg);
-	char nickname[50];
-	read(client_socket, nickname, sizeof(nickname));
+
+	char nickname[NICKNAME_LEN];
+	if(read(client_socket, nickname, sizeof(nickname)) <= 0) {
+		close(client_socket);
+		pthread_exit(NULL);
+	}
+
 	nickname[strcspn(nickname, "\n")] = 0;
 	add_client(client_socket, nickname);
-	char buffer[1024];
 
+	char buffer[BUFFER_SIZE];
 	while(1) {
 		ssize_t bytes_received = read(client_socket, buffer, sizeof(buffer));
 		if (bytes_received <= 0) break;
-		const char *sender_name = "Unknown";
-		for(int i = 0; i < client_count; i++) {
-			if(clients[i].socket == client_socket) {
-				sender_name = clients[i].nickname;
-				break;
-			}
-		}
-		printf("\n[SERVER] Received message from %s: %s\n", sender_name, buffer);
+		printf("[SERVER] Received message from %s: %s\n", get_nickname(client_socket), buffer);
 		broadcast_message(buffer, client_socket);
 	}
 
+	printf("[SERVER] %s disconnected.\n", get_nickname(client_socket));
 	remove_client(client_socket);
 	close(client_socket);
 	pthread_exit(NULL);
@@ -89,44 +99,45 @@ void *handle_client(void *arg) {
 
 void handle_sigint(int sig){
 	printf("\n[SERVER] Caught Ctrl+C - shutting down... \n");
-	close(sock);
+	close(server_socket);
 	exit(0);
 }
 
 int main(void) {
 	signal(SIGINT, handle_sigint);
-	struct sockaddr_in serv_name;
 
-	sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock < 0) {
-		perror("Error opening channel");
-		close(sock);
+	struct sockaddr_in serv_name;
+	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_socket < 0) {
+		perror("Error creating socket");
 		exit(1);
 	}
 
 	int opt = 1;
-	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+	setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-	bzero(&serv_name, sizeof(serv_name));
+	memset(&serv_name, 0, sizeof(serv_name));
 	serv_name.sin_family = AF_INET;
 	serv_name.sin_addr.s_addr = htonl(INADDR_ANY);
 	serv_name.sin_port = htons(PORT);
 
-	if(bind(sock, (struct sockaddr *)&serv_name, sizeof(serv_name)) < 0) {
-		perror("Error naming channel");
-		close(sock);
+	if(bind(server_socket, (struct sockaddr *)&serv_name, sizeof(serv_name)) < 0) {
+		perror("Error binding socket");
+		close(server_socket);
 		exit(1);
 	}
 
-	listen(sock,1);
+	listen(server_socket, 10);
+	printf("[SERVER] Listening on port %d...\n", PORT);
 
 	while(1) {
 		struct sockaddr_in client_addr;
 		socklen_t client_len = sizeof(client_addr);
 
 		int *client_sock = malloc(sizeof(int));
-		*client_sock = accept(sock, (struct sockaddr *)&client_addr, &client_len);
+		if(!client_sock) continue;
 
+		*client_sock = accept(server_socket, (struct sockaddr *)&client_addr, &client_len);
 		if(*client_sock < 0){
 			perror("Error accepting client");
 			free(client_sock);
@@ -138,7 +149,7 @@ int main(void) {
 		pthread_detach(tid);
 	}
 
-	close(sock);
+	close(server_socket);
 	return 0;
 }
 
